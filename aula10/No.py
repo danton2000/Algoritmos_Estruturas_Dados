ec123d2b26417c390790954b95992527fd7e2a8a
Danton Rodrigues <dantoncampos@hotmail.com>
2024-05-27 21:52:00 -0300

    Subindo script da aula 10

---
+++ aula10/No.py
@@ -0,0 +1,9 @@
+class No:
+    
+    def __init__(self, valor):
+        
+        self.dado = valor
+        
+        self.proximo = None
+        
+        
---
+++ aula10/main.py
@@ -0,0 +1,22 @@
+from  Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+
+lista.adicionarNoFim("João")
+
+lista.adicionarNoFim("Marina")
+
+lista.adicionarNoFim("Daniel")
+
+lista.adicionarNoInicio("Gojo")
+
+lista.removerDoInicio()
+
+lista.removerDoFim()
+
+lista.removerDoFim()
+
+lista.removerDoFim()
+
